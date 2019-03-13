450f86a8b7a5535aecca1c40c9447f8bc4a55d36
Jeffernic <44542012+Jeffernic@users.noreply.github.com>
2019-03-13 18:14:23 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 $gl=(isset($_COOKIE['gl']) && $_COOKIE['gl'])?$_COOKIE['gl']:'HK';
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyCjSpc90W81GoukPDdTD2Q3BufT44M__Ik');
+define('APIKEY', 'AIzaSyAQso-vNWnJxk2d2mlbJ2EXOPmoidVFpZo');
 define('GJ_CODE', $gl);
 define('SITE_NAME', 'You2be');
 define('TITLENAME', 'Youtoolbe');
